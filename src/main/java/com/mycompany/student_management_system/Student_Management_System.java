618b4eedde338976aae7f9ebbbaa3d5ac11fca35
Sebastian Rafael <Sebastian Rafael@DESKTOP-EFJJ7PS>
2024-11-27 15:43:57 +0800

    "UPDATE"

--- src/main/java/com/mycompany/student_management_system/Student_Management_System.java
+++ src/main/java/com/mycompany/student_management_system/Student_Management_System.java
@@ -1,12 +1,18 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.student_management_system;
 
+/**
+ *
+ * @author Sebastian Rafael
+ */
 public class Student_Management_System {
-
     public static void main(String[] args) {
-      
-        Student_Class run = new Student_Class ();
-        run.setVisible (true);
         
+        Student_Class run = new Student_Class();
+        run.setVisible(true);
     }
+  
 }
