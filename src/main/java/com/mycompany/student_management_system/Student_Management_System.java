7399268bc3980e378a831fc69dfc7da83afd7310
Sebastian Rafael <Sebastian Rafael@DESKTOP-EFJJ7PS>
2025-01-02 09:31:57 +0800

    Update of Login, menu, and register as f January 2, 2025

--- src/main/java/com/mycompany/student_management_system/Student_Management_System.java
+++ src/main/java/com/mycompany/student_management_system/Student_Management_System.java
@@ -11,8 +11,9 @@ package com.mycompany.student_management_system;
 public class Student_Management_System {
     public static void main(String[] args) {
         
-        Student_Class run = new Student_Class();
-        run.setVisible(true);
+        Login_Frame runFrame = new Login_Frame();
+        runFrame.setVisible(true);
+        
     }
   
 }
