8442dcd4119ac3f401710fa46bcf60cdcbaa76e4
USER <USER@Eliana-Grace>
2025-01-06 19:34:30 +0800

    FFF

--- src/main/java/com/mycompany/student_management_system/Student_Management_System.java
+++ src/main/java/com/mycompany/student_management_system/Student_Management_System.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -9,11 +9,12 @@ package com.mycompany.student_management_system;
  * @author Sebastian Rafael
  */
 public class Student_Management_System {
+
     public static void main(String[] args) {
 
-        Menu_Frame run = new  Menu_Frame();
-        run.setVisible (true);
-        
+        Student_Class run = new Student_Class();
+        run.setVisible(true);
+
     }
-  
+
 }
