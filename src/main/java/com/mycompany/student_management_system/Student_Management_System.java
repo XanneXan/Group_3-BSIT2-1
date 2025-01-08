949064ea2e999515f096d16482ee6e3a8deb418c
Admin <Admin@DESKTOP-CLM9QBN>
2025-01-08 20:34:28 +0800

    Update Student, register, and login database

--- src/main/java/com/mycompany/student_management_system/Student_Management_System.java
+++ src/main/java/com/mycompany/student_management_system/Student_Management_System.java
@@ -1,23 +1,18 @@
-    /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.student_management_system;
 
-import javax.security.auth.login.LoginContext;
+package com.mycompany.student_management_system;
 
 /**
  *
  * @author Sebastian Rafael
  */
+
 public class Student_Management_System {
 
     public static void main(String[] args) {
 
-        Student_Class run = new Student_Class();
+        Login_Frame run = new Login_Frame();
         run.setVisible(true);
         
-
     }
 
 }
